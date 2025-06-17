40ae9d0cf3e05021febed88503f4c4e6ac692605
Maksym Chesherov <chesherow22d@gmail.com>
2025-06-17 14:46:13 +0300

    created main file

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from app.api.episode import router as episode_router
+from app.config import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await db_helper.dispose()
+
+main_app = FastAPI(title="Podcast API", lifespan=lifespan)
+main_app.include_router(episode_router)
+
+@main_app.get("/")
+async def root():
+    return {"message": "Hi"}
