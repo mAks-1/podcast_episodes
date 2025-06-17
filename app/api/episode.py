640bec8c31a7ea259af45cc93b1db61c1bc608cc
Maksym Chesherov <chesherow22d@gmail.com>
2025-06-17 13:55:51 +0300

    created api for getting all episodes

---
+++ app/api/episode.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+
+from app.config import db_helper
+from app.models.podcast_episode import PodcastEpisodeModel
+from app.schemas.podcast_episode import PodcastEpisode, PodcastEpisodeCreate
+from app.crud import podcast_episode as crud_podcast_episode
+
+router = APIRouter(prefix="/episodes", tags=["Episodes"])
+
+
+
+@router.get("/", response_model=list[PodcastEpisode])
+async def get_episodes(session: Annotated[AsyncSession, Depends(db_helper.session_getter)],):
+    episodes = await crud_podcast_episode.get_all_episodes(session=session)
+    return episodes if episodes else []
+
+@router.post("/", response_model=PodcastEpisode, status_code=status.HTTP_201_CREATED)
+async def create_episode(session: Annotated[AsyncSession, Depends(db_helper.session_getter)],):
+    pass
