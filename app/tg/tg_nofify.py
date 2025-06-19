d7ed513deb1c3ca87a5bae43f3beaa2be1106fc2
Maksym Chesherov <chesherow22d@gmail.com>
2025-06-19 13:16:14 +0300

    created func for tg notification

---
+++ app/tg/tg_nofify.py
@@ -0,0 +1,20 @@
+import os
+import aiohttp
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+async def send_telegram_notification(message: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML",
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=payload) as resp:
+            if resp.status != 200:
+                text = await resp.text()
+                raise RuntimeError(f"Telegram error {resp.status}: {text}")
